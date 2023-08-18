4682a91db6dd6378f55a0fd19dfe9f255c543101
Divyanshi <divyansh.rakshit@gmail.com>
2023-08-19 01:55:09 +0530

    Added a contact me section with linkedin

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -2,6 +2,7 @@ function ContactMe() {
     return (
         <div className="contact-me container-fluid">
             <h1 className="display-1 caveat white">Contact <span className='blue'>Me</span></h1>
+            <button><a href="https://www.linkedin.com/in/divyanshi-srivastava-ds/"><img src='static/Icons/linkedin.svg' className="img img-fluid" /></a></button>
         </div>
     )
 }
