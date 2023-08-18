a1865e3750bd49fb8bfaf7348fbf769dd52ab67e
Divyanshi <divyansh.rakshit@gmail.com>
2023-08-18 15:50:02 +0530

    Modified timeline css.

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,6 +1,10 @@
+import './Styles.scss'
+
 function Skills() {
     return (
-        <></>
+        <div className='skills container-fluid'>
+            <h1 className='display-1 caveat white'>My <span className='yellow'>Skills</span></h1>
+        </div>
     )
 }
 
--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -9,7 +9,7 @@ function Timeline() {
                         <img src="src/assets/me-google1.jpeg" className="card-img-top"/>
                         <div className="card-body p-4">
                             <h4>Google: SWE Intern</h4>
-                            <p className="text-muted"><i className="bi-calendar-month"></i> May 2023 - July 2023</p>
+                            <p className="date"><i className="bi-calendar-month"></i> May 2023 - July 2023</p>
                             <p className="mb-0">Worked under the Workspace Security Team at Google Cloud, on a pivotal front-end based project focused on enhancing the Security Investigation Tool by implementing vital features that were in high demand by our valued customers.</p>
                         </div>
                     </div>
@@ -19,7 +19,7 @@ function Timeline() {
                         <img src="src/assets/me-google2.jpeg" className="card-img-top"/>
                         <div className="card-body p-4">
                             <h4>Google: STEP Intern</h4>
-                            <p className="text-muted"><i className="bi-calendar-month"></i> May 2022 - August 2022</p>
+                            <p className="date"><i className="bi-calendar-month"></i> May 2022 - August 2022</p>
                             <p className="mb-0">Worked under the Formats Team at Google Ads, on the back-end of a project which aimed at creating a tool/platform to support faster prototyping and experimentation of new Ad Formats/Ad Extensions.</p>
                         </div>
                     </div>
