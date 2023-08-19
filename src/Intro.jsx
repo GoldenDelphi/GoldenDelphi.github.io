b50ff1bff99f0dbeefe0c3957f9340bcb69696ef
Divyanshi <divyansh.rakshit@gmail.com>
2023-08-19 12:18:44 +0530

    Added image in intro.

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -3,13 +3,25 @@ import './Styles.scss'
 function Intro() {
     return (
         <div className='intro'>
-            <div id='intro-text'>
-                <h1 className='display-1 white caveat'>Hi!<br/>I'm
-                <span className='pink'> Divyanshi</span>
-                </h1>
-                <h3 className='display-5 muted-white'>Software Engineering Intern</h3>
-                {/* <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cursus feugiat velit, vel ullamcorper risus tincidunt quis</h3> */}
+            <div className='row'>
+                <div className='col-12 col-md-8 box-1'>
+                    <div id='intro-text'>
+                        <h1 className='display-1 white caveat'>Hi!<br />I'm
+                            <span className='pink'> Divyanshi</span>
+                        </h1>
+                        <h3 className='display-5 muted-white'>Software Engineering Intern</h3>
+
+                        {/* <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cursus feugiat velit, vel ullamcorper risus tincidunt quis</h3> */}
+                    </div>
+                </div>
+                <div className='col-12 col-md-4 box-2 text-center'>
+                    <div className='intro-img-div'>
+                        <img src="static/me-nobg1.png" className='intro-img img-fluid' />
+                    </div>
+
+                </div>
             </div>
+
         </div>
     )
 }
