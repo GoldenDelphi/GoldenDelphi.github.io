b5614c6aa3310c065dcf39a87e834e98c9e7c8ce
Divyanshi <divyansh.rakshit@gmail.com>
2023-08-20 22:26:05 +0530

    Added a footer for linkedin.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,15 @@
 import '../assets/App.scss'
+import Footer from './Footer'
 
 export default function About() {
     return (
+        <>
         <div className="about-comp container-fluid">
             <p className="fs-5">Hi! I'm Divyanshi Srivastava</p>
             <h1 className="">Software Engineering Intern</h1>
             <h3 className="">Crafting Code with Passion: Building Tomorrow's Innovations.</h3>
         </div>
+        <Footer />
+        </>
     )
 }   
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div className="footer-comp container-fluid sticky-bottom text-end">
+            <a href="https://www.linkedin.com/in/divyanshi-srivastava-ds/">
+                <img src="../linkedin.svg" className="img-soc" alt="linkedin" />
+            </a>
+        </div>
+    )
+}
